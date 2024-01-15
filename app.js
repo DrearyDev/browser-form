accf727bbfcee1b58b10af2415f8395f8341837d
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-01-15 12:15:13 -0500

    Add basic Form and styling without functionallity

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const showBtn = document.querySelector('button');
+const dialog = document.querySelector('dialog');
+const submitBtn = document.querySelector('form > button');
+
+showBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+
 
