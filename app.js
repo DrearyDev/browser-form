0169643d71a3c9147cff5fb8fd7d5e8c697c6f6d
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-01-16 14:25:22 -0500

    Add Zip Code Validation

--- app.js
+++ app.js
@@ -20,23 +20,75 @@ showBtn.addEventListener('click', () => {
 });
 
 
+function checkZIP() {
+    const constraints = {
+        Switzerland: [
+            "^(CH-)?\\d{4}$",
+            "Switzerland ZIPs must have 4 digits: e.g. CH-1950 or 1950",
+            "CH-1950 or 1950"
+        ],
+        France: [
+            "^(F-)?\\d{5}$",
+            "France ZIPs must have 5 digits: e.g. F-75012 or 75012",
+            "F-75012 or 75012"
+        ],
+        Germany: [
+            "^(D-)?\\d{5}$",
+            "Germany ZIPs must have 5 digits: e.g. D-12345 or 12345",
+            "D-12345 or 12345"
+        ],
+        Netherlands: [
+            "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+            "Netherland ZIPs must have 4 digits, then 2 letters except SA, SD and SS",
+            "1234SD"
+        ]
+    };
+
+    const constraint = new RegExp(constraints[countryInput.value][0], '');
+
+    if (zCodeInput.value === '') {
+        return false;
+    }
+    else if (constraint.test(zCodeInput.value)) {
+        zCodeInput.style.borderColor = 'green';
+        zCodeInput.setCustomValidity('');
+        zCodeInput.nextElementSibling.innerText = zCodeInput.validationMessage;
+    } else {
+        zCodeInput.style.borderColor = 'red';
+        zCodeInput.setCustomValidity(constraints[countryInput.value][1]);
+        zCodeInput.nextElementSibling.innerText = zCodeInput.validationMessage;
+        return false;
+    };
+
+    return true;
+};
+
 
 
 function validate() {
     let valid = true;
 
     inputs.forEach(input => {
-        console.log(input.validity);
-
+        //all input validation
         if (input.checkValidity()) {
-            console.log('test');
+            input.style.borderColor = 'green';
             input.nextElementSibling.innerText = '';
         } else {
-            console.log('failed');
+            input.style.borderColor = 'red';
             input.nextElementSibling.innerText = input.validationMessage;
             valid = false;
         };
     });
+
+    //zip code validation
+    if (!checkZIP()) {
+        valid = false;
+    };
+
+    //password validation
+    if (!checkPassword()) {
+        valid = false;
+    };
 
     return valid;
 };
